5dbf681e32abdc2493489e311b7b6d04c8facfc8
mjx1993 <464471063@qq.com>
2017-10-09 00:33:47 +0800

    1009

--- mvc-demo/app.js
+++ mvc-demo/app.js
@@ -1,7 +1,19 @@
-const controller = require('./controller');
+//
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
 
 const app = new Koa();
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// parse request body:
+app.use(bodyParser());
 
 app.use(controller());
 
