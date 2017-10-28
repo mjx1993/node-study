6eb536598370861b260ee5782b13cf0ae3e1e331
mjx1993 <464471063@qq.com>
2017-10-28 19:01:55 +0800

    用户管理

--- sinn-demo/rest/controllers/backend/index.js
+++ sinn-demo/rest/controllers/backend/index.js
@@ -2,6 +2,8 @@
 * @ author unoboy
 * @ use 管理后台文章相关接口逻辑层
 */
+import mongoose from 'mongoose';
+import menu from '../config/menu';
 
 class BackendMain {
     //登录页渲染
@@ -14,6 +16,22 @@ class BackendMain {
     	console.log(ctx.flash);
     	const user = ctx.session.user;
     	return ctx.render('home.html', { title: 'Unoboy管理平台',message:'这里是首页',user });
+    }
+
+    //用户管理
+    static async user(ctx) {
+        let Index = 1;
+        const { type,item } = ctx.query;
+        if (item) Index = item;
+        const user = ctx.session.user;
+        if (user == '') return ctx.render('login.html', { title: 'Unoboy管理平台'});
+        return ctx.render('user.html', {
+            title: 'Unoboy管理平台',
+            message: '用户管理',
+            user,
+            index: Index,
+            menu: menu[type]
+        })
     }
 }
 
