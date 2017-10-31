c64f300d3d70c045acc74d746683e3a5bcb82091
mjx1993 <464471063@qq.com>
2017-10-31 18:45:45 +0800

    add elm-manage

---
+++ elm-manage-demo/src/page/login.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="login_page fillcontain">
+        <transition name="form-fade" mode="in-out">
+            <section class="form_contianer" v-show="showLogin">
+		  		<div class="manage_tip">
+		  			<p>后台管理系统</p>
+		  		</div>
+            </section>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+    @import '../style/mixin';
+	.login_page{
+		background-color: #324057;
+    }
+    .manage_tip{
+		position: absolute;
+		width: 100%;
+		top: -100px;
+		left: 0;
+		p{
+			font-size: 34px;
+			color: #fff;
+		}
+	}
+	.form_contianer{
+		.wh(320px, 210px);
+		.ctp(320px, 210px);
+		padding: 25px;
+		border-radius: 5px;
+		text-align: center;
+		background-color: #fff;
+		.submit_btn{
+			width: 100%;
+			font-size: 16px;
+		}
+	}
+	.tip{
+		font-size: 12px;
+		color: red;
+	}
+</style>
+
+
--- sinn-demo/rest/models/user/admin.model.js
+++ sinn-demo/rest/models/user/admin.model.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AdminUserSchema = new Schema({
-  name: { type: String, required: true }, // 用户名
-  nickname: { type: String, required: true }, // 昵称
+  name: { type: String, required: true },
+  nickname: { type: String, required: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
