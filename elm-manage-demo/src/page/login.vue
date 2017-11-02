43dad3909b468dd4c47f101394798442c5c4c7ff
mjx1993 <464471063@qq.com>
2017-11-02 19:10:55 +0800

    elm

--- elm-manage-demo/src/page/login.vue
+++ elm-manage-demo/src/page/login.vue
@@ -5,14 +5,64 @@
 		  		<div class="manage_tip">
 		  			<p>后台管理系统</p>
 		  		</div>
+				<el-form :model="loginForm" status-icon :rules="rules" ref="loginForm">
+					<el-form-item prop="username">
+						<el-input v-model="loginForm.username" placeholder="用户名"></el-input>
+					</el-form-item>
+					<el-form-item prop="password">
+						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
+					</el-form-item>
+					<el-form-item>
+				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
+				  	</el-form-item>
+				</el-form>
+				<p class="tip">温馨提示：</p>
+				<p class="tip">未登录过的新用户，自动注册</p>
+				<p class="tip">注册过的用户可凭账号密码登录</p>
             </section>
         </transition>
     </div>
 </template>
 
 <script>
+import {login, getAdminInfo} from '@/api/getData'
+import {mapActions, mapState} from 'vuex'
+
 export default {
-    
+    data() {
+		return {
+			loginForm: {
+				username: '',
+				password: ''
+			},
+			rules: {
+				username: [
+			        { required: true, message: '请输入用户名', trigger: 'blur' },
+			    ],
+				password: [
+					{ required: true, message: '请输入密码', trigger: 'blur' }
+				],
+			},
+			showLogin: false,
+		}
+	},
+	mounted() {
+		this.showLogin = true;
+	},
+	computed: {
+		...mapState(['adminInfo']),
+	},
+	methods: {
+		async submitForm(formname) {
+			this.$refs[formname].validate(async (valid) => {
+				if (valid) {
+
+				} else {
+
+				}
+			})
+		}
+	}
 }
 </script>
 
---
+++ elm-manage-demo/src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = {
+	adminInfo: {
+		avatar: 'default.jpg'
+	},
+}
+
+const mutations = {
+	
+}
+
+const actions = {
+    
+}
+
+export default new Vuex.Store({
+	state,
+	actions,
+	mutations,
+})
